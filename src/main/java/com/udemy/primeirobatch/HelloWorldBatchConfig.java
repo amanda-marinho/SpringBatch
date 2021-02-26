7c6d4351fdcd6c09367c3446e6c4ae1499d843e0
Amanda Marinho <amanda.marinho@usp.br>
2021-02-25 23:59:08 -0300

    Primeiro Batch

---
+++ src/main/java/com/udemy/primeirobatch/HelloWorldBatchConfig.java
@@ -0,0 +1,50 @@
+package com.udemy.primeirobatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class HelloWorldBatchConfig {
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloWorld() {
+        return jobBuilderFactory
+                .get("HelloWorldJob")
+                .start(printHelloWorldStep())
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    public Step printHelloWorldStep() {
+        return stepBuilderFactory
+                .get("printHelloWorldStep")
+                .tasklet(printHelloWorldTasklet())
+                .build();
+    }
+
+    public Tasklet printHelloWorldTasklet() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("Hello, World!");
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+}
